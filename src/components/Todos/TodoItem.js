fea05a1f81bab6dc648e1a0fea89f36fa10ccc59
Kivanc Eski <eskikivanc@gmail.com>
2022-09-17 16:23:30 +0300

    Add 75 letters

--- src/components/Todos/TodoItem.js
+++ src/components/Todos/TodoItem.js
@@ -3,7 +3,10 @@ import cancel from '../../assets/cancel.svg';
 const TodoItem = () => {
   return (
     <div className="bg-slate-100 hover:bg-slate-200 rounded-xl px-6 py-3 text-center text-gray-900 border border-gray-900">
-      <h6 className="text-lg mb-3">Todo Content</h6>
+      <h6 className="text-lg mb-3 break-words max-h-50">
+        Lorem ipsum dolor sit amet consectetur
+        adisfdsdfaaasdfsdfsdfadfsafdfsafdsfa
+      </h6>
       <span className="text-base">Todo Completed</span>
       <img
         src={cancel}
